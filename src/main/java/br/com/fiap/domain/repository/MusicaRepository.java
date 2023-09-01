fddc0aecf8248e1f53380cf95b5bd51df9a0c248
Angelica Melo <angellmello1993@gmail.com>
2023-08-31 23:50:32 -0300

    Correção - Implementando ajustes na classe MusicaRepository.

--- src/main/java/br/com/fiap/domain/repository/MusicaRepository.java
+++ src/main/java/br/com/fiap/domain/repository/MusicaRepository.java
@@ -2,26 +2,49 @@ package br.com.fiap.domain.repository;
 
 import br.com.fiap.domain.entity.Musica;
 
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 
-public class MusicaRepository implements Repository<Musica, Integer> {
+public class MusicaRepository implements Repository<Musica, Long> {
+
+    private Set<Musica> musicas;
+
+    public MusicaRepository() {
+        musicas = new LinkedHashSet<>();
+    }
+
     @Override
     public List<Musica> findAll() {
-        return null;
+
+        return musicas.stream().toList();
     }
 
     @Override
     public Musica findById(Integer id) {
-        return null;
+
+        return musicas.stream()
+                .filter( m -> m.getId().equals( id ) )
+                .findFirst()
+                .orElse( null );
+
     }
 
     @Override
     public List<Musica> findByName(String texto) {
-        return null;
+
+        return musicas.stream()
+                .filter( m -> m.getNome().toLowerCase().contains( texto.toLowerCase() ) )
+                .toList();
     }
 
     @Override
     public Musica persist(Musica musica) {
-        return null;
+
+        if (Objects.isNull( musica )) return null;
+        if (Objects.isNull( musica.getId() )) musica.setId( musicas.size() + 1L );
+        musicas.add( musica );
+        return musica;
     }
 }
