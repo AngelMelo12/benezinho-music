300bf55e4dc20e4c6f56bf43ed5c038f498ae067
Angelica Melo <angellmello1993@gmail.com>
2023-08-31 23:40:22 -0300

    Correção - Implementando ajustes na classe ArtistaRepository.

--- src/main/java/br/com/fiap/domain/repository/ArtistaRepository.java
+++ src/main/java/br/com/fiap/domain/repository/ArtistaRepository.java
@@ -3,27 +3,47 @@ package br.com.fiap.domain.repository;
 import br.com.fiap.domain.entity.Artista;
 import br.com.fiap.domain.service.Service;
 
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 
-public class ArtistaRepository implements Repository<Artista, Integer> {
+public class ArtistaRepository implements Repository<Artista, Long> {
+
+    private Set<Artista> artistas;
+
+    public ArtistaRepository() {
+        artistas = new LinkedHashSet<>();
+    }
+
     @Override
     public List<Artista> findAll() {
-        return null;
+        return artistas.stream().toList();
     }
 
     @Override
     public Artista findById(Integer id) {
-        return null;
+
+        return artistas.stream()
+                .filter( a->a.getId().equals( id ) )
+                .findFirst().orElse( null );
     }
 
     @Override
     public List<Artista> findByName(String texto) {
-        return null;
+
+        return artistas.stream()
+                .filter( a -> a.getNome().toLowerCase().contains( texto.toLowerCase() ) )
+                .toList();
     }
 
     @Override
     public Artista persist(Artista artista) {
-        return null;
+
+        if (Objects.isNull( artista )) return null;
+        if (Objects.isNull( artista.getId() )) artista.setId( artistas.size() + 1L );
+        artistas.add( artista );
+        return artista;
     }
 
 
