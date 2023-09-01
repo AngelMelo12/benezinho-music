a04b5bee0b79581bf4ba5e64947ed47081639012
Angelica Melo <angellmello1993@gmail.com>
2023-08-31 23:30:53 -0300

    Correção - Implementando ajustes na classe EstiloRepository.

--- src/main/java/br/com/fiap/domain/repository/EstiloRepository.java
+++ src/main/java/br/com/fiap/domain/repository/EstiloRepository.java
@@ -2,27 +2,41 @@ package br.com.fiap.domain.repository;
 
 import br.com.fiap.domain.entity.Estilo;
 
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 
-public class EstiloRepository implements Repository<Estilo, Integer>{
+public class EstiloRepository implements Repository<Estilo, Long>{
+
+    private Set<Estilo>estilos; //Guardando os objetos numa coleção.
+
+    public EstiloRepository() {
+        estilos = new LinkedHashSet<>(); //Inicializando a coleção de estilos.
+    }
 
     @Override
     public List<Estilo> findAll() {
-        return null;
+        return estilos.stream().toList();
     }
 
     @Override
     public Estilo findById(Integer id) {
-        return null;
+
+        return estilos.stream().filter( e -> e.getId().equals( id ) ).findFirst().orElse( null );
     }
 
     @Override
     public List<Estilo> findByName(String texto) {
-        return null;
+
+        return estilos.stream().filter( e -> e.getNome().toLowerCase().contains( texto.toLowerCase() )).toList();
     }
 
     @Override
     public Estilo persist(Estilo estilo) {
-        return null;
+        if (Objects.isNull( estilo )) return null;
+        if (Objects.isNull( estilo.getId() )) estilo.setId(estilos.size() + 1L);
+        estilos.add( estilo );
+        return estilo;
     }
 }
